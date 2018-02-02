443ab12922d9b173ceff1350d8768a926972806d
Roberto Loreto <rloreto@gmail.com>
2018-02-02 12:13:03 +0100

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
 require('babel-core/register')
-//process.env.NODE_ENV = 'development'
-process.env.MASTER_KEY = '12F2G71AD432F69CB1281F7117B0'
-process.env.JWT_SECRET = 'xsmyfapujvD96PijUHfgBare1r5ofEoZKJiGRCg636V42oPlUHIyNveqItJfdqs'
-process.env.MONGODB_URI = 'mongodb://quierobesarte:c100markii@ds161041.mlab.com:61041/qbapi'
 
 exports = module.exports = require('./src/app')
